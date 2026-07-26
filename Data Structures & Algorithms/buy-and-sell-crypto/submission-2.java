aa628417c06daa05eec51e41bffe6a1f25b16559
Indresh Kotha <kothaindresh44@gmail.com>
2026-07-27 00:12:34 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l=0,r=0,maxp=0;
+        while(r<prices.length)
+        {
+            if(prices[r]>prices[l])
+            {
+                maxp = Math.max(maxp,prices[r]-prices[l]);
+                r++;
+            }
+            else{
+                l=r;
+                r++;
+            }
+        }
+        return maxp;
+    }
+}
